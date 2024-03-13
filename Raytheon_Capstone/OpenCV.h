2204265f5d374fe857326503057a06c4a4f995c5
Zachamus <stevenreiber@ucsb.edu>
2024-03-12 17:07:51 -0700

    Updated OpenCV class

---
+++ Raytheon_Capstone/OpenCV.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <Windows.h> //swap this out lol obv not using windows API 
+#include <vector>
+#include <opencv2/objdetect/aruco_detector.hpp>
+
+
+class OpenCV
+{
+	public: 
+		BOOL targetFound;
+
+};
+
