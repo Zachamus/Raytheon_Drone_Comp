ef416ebf49d4b305400697a0ecd9ec0f8e1d7260
Zachamus <stevenreiber@ucsb.edu>
2024-04-05 14:48:49 -0700

    Updated Move header

--- Raytheon_Capstone/Move.h
+++ Raytheon_Capstone/Move.h
@@ -1,1 +1,17 @@
 #pragma once
+
+#include <mavsdk/mavlink_address.h>
+#include <mavsdk/mavlink/common/mavlink.h>
+#include <vector>
+
+
+
+namespace Move {
+	std::vector<int> MavSDKLocalMove(std::vector<int> local_dist);
+
+	std::vector<int> MavSDKGlobalMove(std::vector<int> global_pos);
+
+	std::vector<int> dist;
+
+	std::vector<int> acceptable;
+}
