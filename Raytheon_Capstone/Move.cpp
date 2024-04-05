845935edf47c66b4cfe51e3e13b839142a8c0046
Zachamus <stevenreiber@ucsb.edu>
2024-04-05 14:46:36 -0700

    Start of Move etc

---
+++ Raytheon_Capstone/Move.cpp
@@ -0,0 +1,21 @@
+#include "Move.h"
+
+namespace Move {
+	std::vector<int> MavSDKLocalMove(std::vector<int> local_dist) {
+		std::vector<int> gpsCoordsCurr;
+
+
+		return gpsCoordsCurr;
+	}
+
+	std::vector<int> MavSDKGlobalMove(std::vector<int> global_pos) {
+		std::vector<int> gpsCoordsCurr;
+
+
+		return gpsCoordsCurr;
+	}
+
+	std::vector<int> dist;
+
+	std::vector<int> acceptable;
+}
