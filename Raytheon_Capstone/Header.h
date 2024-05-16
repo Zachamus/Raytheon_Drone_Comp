2f88288e3497710dbf1e731b2cdf3559fe8e6251
stevenreiber <stevenreiber@ucsb.edu>
2024-05-15 18:50:05 -0700

    Updated with other branch

---
+++ Raytheon_Capstone/Header.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <iostream>
+#include <ctime>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <stdio.h>
+#include <cstdlib>
+#include "opencv2/opencv.hpp"
+#include <opencv2/highgui.hpp>
+#include <opencv2/objdetect/aruco_detector.hpp>
+#include <iostream>
+#include <opencv2/highgui.hpp>
+#include <opencv2/objdetect/aruco_detector.hpp>
+#include <opencv2/calib3d.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/video.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <ctime>
+#include <stack>
+#include <iostream>
+#include <math.h>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/videoio.hpp"
+#include <opencv2/video.hpp>
+#include "opencv2/imgcodecs.hpp"
+#include <time.h>
+#include <mavsdk/plugins/offboard/offboard.h>
+
+
+
+#define ASSERT_EQ(val1, val2) \
+    if (!((val1) == (val2))) { \
+        throw std::runtime_error("Assertion failed: " ); \
+    }
+
+
+#define ASSERT_TRUE(condition) \
+    if (!(condition)) { \
+        throw std::runtime_error("Assertion failed: " #condition); \
+    }
--- Raytheon_Capstone/Raytheon_Capstone.cpp
+++ Raytheon_Capstone/Raytheon_Capstone.cpp
@@ -1,18 +1,29 @@
 // Raytheon_Capstone.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
-#include <iostream>
-#include <cassert>
-#include <vector>
-#include <thread>
-#include "Move.h"
+#include "Header.h"
+#include <string>
 #include <mavsdk/plugins/offboard/offboard.h>
-#include <mavsdk/mavsdk.h>
-#include <ctime>
 #include <mavsdk/plugins/action/action.h>
+#include <mavsdk/mavsdk.h>
+#include <mavsdk/plugins/telemetry/telemetry.h>
+#include <iostream>
+#include <future>
+#include <memory>
+#include <thread>
+#include <mavsdk/geometry.h>
 
 
+mavsdk::geometry::CoordinateTransformation::LocalCoordinate cv_to_mav(std::vector<float>, double heading) {
+	// convert vector to Local Coordinate struct
+	//takes x y z vector and compass heading
+}
 
+
+using namespace cv;
+using namespace std;
+using namespace mavsdk;
+using std::chrono::seconds;
+using std::this_thread::sleep_for;
 
 enum states {
 	init,
@@ -22,8 +33,36 @@ enum states {
 	reset
 }curr_state;
 
+namespace {
+	const char* about = "Basic marker detection";
 
+	//! [aruco_detect_markers_keys]
+	const char* keys =
+		"{d        | 0     | dictionary: DICT_4X4_50=0, DICT_4X4_100=1, DICT_4X4_250=2,"
+		"DICT_4X4_1000=3, DICT_5X5_50=4, DICT_5X5_100=5, DICT_5X5_250=6, DICT_5X5_1000=7, "
+		"DICT_6X6_50=8, DICT_6X6_100=9, DICT_6X6_250=10, DICT_6X6_1000=11, DICT_7X7_50=12,"
+		"DICT_7X7_100=13, DICT_7X7_250=14, DICT_7X7_1000=15, DICT_ARUCO_ORIGINAL = 16,"
+		"DICT_APRILTAG_16h5=17, DICT_APRILTAG_25h9=18, DICT_APRILTAG_36h10=19, DICT_APRILTAG_36h11=20}"
+		"{cd       |       | Input file with custom dictionary }"
+		"{v        |       | Input from video or image file, if ommited, input comes from camera }"
+		"{ci       | 0     | Camera id if input doesnt come from video (-v) }"
+		"{c        |       | Camera intrinsic parameters. Needed for camera pose }"
+		"{l        | 0.1   | Marker side length (in meters). Needed for correct scale in camera pose }"
+		"{dp       |       | File of marker detector parameters }"
+		"{r        |       | show rejected candidates too }"
+		"{refine   |       | Corner refinement: CORNER_REFINE_NONE=0, CORNER_REFINE_SUBPIX=1,"
+		"CORNER_REFINE_CONTOUR=2, CORNER_REFINE_APRILTAG=3}";
 
+	//! [aruco_detect_markers_keys]
+
+	const string refineMethods[4] = {
+		"None",
+		"Subpixel",
+		"Contour",
+		"AprilTag"
+	};
+
+}
 
 
 int Thread2() { //second thread running OpenCV giving results to shared resource that main thread can only view
@@ -38,13 +77,42 @@ int main() {
 	mavsdk::Mavsdk::Configuration config = mavsdk::Mavsdk::Configuration::Configuration(component_type);
 	mavsdk::Mavsdk mavsdk(config);
 	mavsdk::ConnectionResult conn_result = mavsdk.add_serial_connection("serial:///dev/ttyAMA0", 57600);
-	while (mavsdk.systems().size() == 0) {
-		
+
+	if (conn_result != ConnectionResult::Success) {
+		std::cerr << "Connection failed: " << conn_result << '\n';
+		return 1;
 	}
-	std::shared_ptr<mavsdk::System> system = mavsdk.systems()[0];
+
+	while (mavsdk.systems().size() == 0) {
+		std::cout << "Waiting for system to connect..." << std::endl;
+		std::this_thread::sleep_for(1s);
+	}
+
+	std::shared_ptr<mavsdk::System> system = mavsdk.systems().at(0);
 	auto offboard = mavsdk::Offboard{ system };
 	auto action = mavsdk::Action{ system };
+	auto telemetry = mavsdk::Telemetry{ system };
 
+
+	while (telemetry.health_all_ok() == false) {
+		std::cout << "Telemetry not healthy";
+		std::this_thread::sleep_for(1s);
+	}
+	std::cout << "System is ready";
+
+	Action::Result set_altitude = action.set_takeoff_altitude(5.0);
+
+	if (set_altitude != Action::Result::Success) {
+		std::cerr << "Set altitude failed" << std::endl;
+		return 1;
+	}
+
+	Action::Result arm_result = action.arm();
+
+	if (arm_result != Action::Result::Success) {
+		std::cerr << "Arm failed: " << arm_result << std::endl;
+		return 1;
+	}
 
 
 
@@ -52,19 +120,17 @@ int main() {
 	while (1) {
 		switch (curr_state) {
 		case init:
-		{
 			if (reset_in) {
 				curr_state = reset;
 				break;
 			}
 			else {
-				BOOL gpio_init = Init::Gpio_init();
-				BOOL picam_init = Init::picam_init();
+				bool gpio_init = Init::Gpio_init();
+				bool picam_init = Init::picam_init();
 				assert(picam_init && gpio_init); //breaks program here if fail values returned
 				curr_state = search;
 				break;
 			}
-		}
 		case search:
 
 		{
@@ -111,7 +177,7 @@ int main() {
 			}
 			else {
 				Drop::MavSDKDescend(Drop::HeightToDrop);
-				BOOL drop_success = Drop::WaterGunFire();
+				bool drop_success = Drop::WaterGunFire();
 				if (drop_success)
 					curr_state = search;
 				curr_state = drop;
@@ -122,7 +188,7 @@ int main() {
 
 		case reset:
 			std::vector<int> pos = Reset::MavSDKReturnHome();
-			BOOL reset_suc = Reset::Reset();
+			bool reset_suc = Reset::Reset();
 
 			if (reset_suc) {
 				//figure out reset logic, probably need to reset whole thing
@@ -136,17 +202,4 @@ int main() {
 
 		}
 	}
-
-	BOOL result_gpio = Init::Gpio_init();
-	BOOL result_picam = Init::picam_init();
-
-	assert(result_gpio && result_picam); //break program if init fail
-
-
-
-
-
-
-
-
 }
