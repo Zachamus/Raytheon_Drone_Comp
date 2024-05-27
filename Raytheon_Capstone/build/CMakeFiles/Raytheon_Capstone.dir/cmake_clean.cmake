586dedceb463f8e36fce3cd9107ae74f2e0271e6
Steven Reiber <stevenreiber@ucsb.edu>
2024-05-27 14:50:47 -0700

    deh

---
+++ Raytheon_Capstone/build/CMakeFiles/Raytheon_Capstone.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Raytheon_Capstone.dir/Raytheon_Capstone.cpp.o"
+  "CMakeFiles/Raytheon_Capstone.dir/Raytheon_Capstone.cpp.o.d"
+  "Raytheon_Capstone"
+  "Raytheon_Capstone.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Raytheon_Capstone.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
